6d37a739d47b666782bad7e69d7084893d38f79f
Kevin Rivera <kevinriveradev@gmail.com>
2021-11-12 01:25:54 +0100

    .

--- pages/api/multimedia.js
+++ pages/api/multimedia.js
@@ -14,10 +14,9 @@ handler.post((req,res) => {
 
 const saveImage = ({ body , files, query } , res) => {
     const rootDir = path.join(__dirname, '..')
-    
+
     try {
         fs.readFile(files.file[0].path , function (err,data) {
-            console.log(`${rootDir}${query.id}.png`)
             fs.writeFile(`${rootDir}${query.id}.png`,data, (err) => {
                 if(err) {
                     console.error(`Error al guardar el fichero: ${err}`)
